28e21ef14c52a55b588495835df00bb93913593b
Mike Bronner <mike@genealabs.com>
2016-08-01 19:32:53 -0700

    Tweak RSS url

--- config/laravel-weblog.php
+++ config/laravel-weblog.php
@@ -2,7 +2,7 @@
 
 return [
     'blog-route-name' => 'blog',
-    'rss-route-name' => 'rss',
+    'rss-route-name' => 'blog/rss',
     'sitemap-route-name' => 'blog/sitemap',
     'user-model' => config('auth.model') ?? config('auth.providers.users.model') ?? null,
     'layout-view' => 'layouts.master',
