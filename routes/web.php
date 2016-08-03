613697b9f0ab33b775ee6101df0a795256c0bdc5
Mike Bronner <mike@genealabs.com>
2016-08-02 20:47:51 -0700

    Put routes in web middleware

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,14 @@ use GeneaLabs\LaravelWeblog\Http\Controllers\Posts;
 use GeneaLabs\LaravelWeblog\Http\Controllers\Rss;
 use GeneaLabs\LaravelWeblog\Http\Controllers\Sitemap;
 
-Route::get(config('genealabs-laravel-weblog.blog-route-name'), Posts::class.'@index');
-Route::get(config('genealabs-laravel-weblog.rss-route-name'), Rss::class.'@index');
-Route::get(config('genealabs-laravel-weblog.sitemap-route-name'), Sitemap::class.'@index');
-Route::resource('posts', Posts::class);
 
-Route::group(['prefix' => 'genealabs/laravel-weblog'], function () {
-    Route::resource('images', Images::class);
+Route::group(['middleware' => ['web']], function () {
+    Route::get(config('genealabs-laravel-weblog.blog-route-name'), Posts::class.'@index');
+    Route::get(config('genealabs-laravel-weblog.rss-route-name'), Rss::class.'@index');
+    Route::get(config('genealabs-laravel-weblog.sitemap-route-name'), Sitemap::class.'@index');
+    Route::resource('posts', Posts::class);
+
+    // Route::group(['prefix' => 'genealabs/laravel-weblog'], function () {
+        Route::resource('genealabs-laravel-weblog-images', Images::class);
+    // });
 });
